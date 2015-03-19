48b1dd3bb887776901cd9a42bbffd3931be472de
machal1989 <maciej.machalski@gmail.com>
2015-03-19 21:50:41 +0100

    Definicja klas oraz prosty program realizujacy polimorfizm

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,9 +3,53 @@
 
 #include "stdafx.h"
 
+using namespace std;
+
+class CWielokat {
+protected:
+	int szerokosc, wysokosc;
+public:
+	void ustaw_wartosci ( int a, int b)
+	{
+		szerokosc = a;
+		wysokosc = b;
+	}
+	virtual int pole () = 0;
+};
+
+class CProstokat: public CWielokat {
+public:
+	int pole ()
+	{
+		return (szerokosc * wysokosc);
+	}
+};
+
+class CTrojkat: public CWielokat {
+public: 
+	int pole ()
+	{
+		return (wysokosc * szerokosc/2);
+	}
+};
+
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	CProstokat prostakat1;
+	CTrojkat troj1;
+
+	CWielokat* wsk1 = &prostakat1;
+	CWielokat* wsk2 = &troj1;
+
+	wsk1->ustaw_wartosci(5,5);
+	wsk2->ustaw_wartosci(6,4);
+
+	cout << wsk1->pole() << endl;
+	cout << wsk2->pole() << endl;
+
+	system("pause");
 	return 0;
 }
 
--- ConsoleApplication1/ConsoleApplication1/stdafx.h
+++ ConsoleApplication1/ConsoleApplication1/stdafx.h
@@ -9,7 +9,8 @@
 
 #include <stdio.h>
 #include <tchar.h>
-
+#include <iostream>
+#include <cstdlib>
 
 
 // TODO: reference additional headers your program requires here
