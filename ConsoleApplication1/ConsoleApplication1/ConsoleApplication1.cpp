ecd64261af575147e3eff250d4c1a1ee374d7e1a
machal1989 <maciej.machalski@gmail.com>
2015-03-19 23:08:47 +0100

    Dodanie nowych klas

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,19 +6,18 @@
 using namespace std;
 
 class CWielokat {
-protected:
-	int szerokosc, wysokosc;
 public:
-	void ustaw_wartosci ( int a, int b)
-	{
-		szerokosc = a;
-		wysokosc = b;
-	}
 	virtual int pole () = 0;
+	virtual int obwod (int a, int b) 
+	{
+		return( a + b );
+	}
 };
 
 class CProstokat: public CWielokat {
+	int szerokosc, wysokosc;
 public:
+	CProstokat(int a, int b): szerokosc(a), wysokosc(b){};
 	int pole ()
 	{
 		return (szerokosc * wysokosc);
@@ -26,7 +25,9 @@ public:
 };
 
 class CTrojkat: public CWielokat {
+	int szerokosc, wysokosc;
 public: 
+	CTrojkat(int a, int b): szerokosc(a), wysokosc(b){};
 	int pole ()
 	{
 		return (wysokosc * szerokosc/2);
@@ -35,19 +36,67 @@ public:
 
 
 
+class Czlowiek {
+public:
+	virtual void pij() = 0;
+	virtual string przedstaw_sie() = 0;
+
+};
+
+
+class Student: public Czlowiek {
+	string imie, nazwisko, d;
+	int NrIndeksu;
+public:
+	Student( string a, string b, string c): imie(a), nazwisko(b), NrIndeksu(c) {};
+	string przedstaw_sie()
+	{
+		return imie + "   " + nazwisko + to_string(NrIndeksu);
+	}
+	void pij(int a)
+	{
+		if(a<=4)
+			d = " drinki";
+		else
+			d = " drinkow";
+		cout << "Jestem studentem moge pic do porzygu, wypije wszystkie " << a << d  << endl;
+	}
+};
+
+class Pracownik: public Czlowiek {
+	string imie, nazwisko;
+public:
+	Pracownik( string a, string b): imie(a), nazwisko(b) {};
+	string przedstaw_sie()
+	{
+		return imie + "   " + nazwisko;
+	}
+	void pij(int a)
+	{
+		if(a<=4)
+			cout << "Wypilem " << a << " drinkow" << endl;
+		else
+			cout << "Moge wypic tylko 4 drinki, pozostale " << (a-4) << " przekazuje mojemu koledze z kolejki" << endl;
+	}
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	CProstokat prostakat1;
-	CTrojkat troj1;
+	int dlugosc;
+	CProstokat prostakat1(3,12);
+	CTrojkat troj1(12,5);
 
 	CWielokat* wsk1 = &prostakat1;
 	CWielokat* wsk2 = &troj1;
 
-	wsk1->ustaw_wartosci(5,5);
-	wsk2->ustaw_wartosci(6,4);
+	
 
 	cout << wsk1->pole() << endl;
 	cout << wsk2->pole() << endl;
+
+	cout << "Podaj dlugosc kolejki" << endl;
+	cin >> dlugosc;
+	//Czlowiek* wsk = new Czlowiek[dlugosc];
 
 	system("pause");
 	return 0;
--- ConsoleApplication1/ConsoleApplication1/stdafx.h
+++ ConsoleApplication1/ConsoleApplication1/stdafx.h
@@ -11,6 +11,7 @@
 #include <tchar.h>
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
 
 // TODO: reference additional headers your program requires here
